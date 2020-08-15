967d1913d4ce6bf346ba448f9e299b4f72f078f5
Nick Brannies <nick.brannies@gmail.com>
2020-08-15 00:42:04 -0700

    Started implementing MySQL database to log message history.

--- cogs/Admin.py
+++ cogs/Admin.py
@@ -1,7 +1,10 @@
+import os
+import dotenv
 import discord
+from datetime import datetime
 from discord.ext import commands
 from discord.ext.commands import has_permissions, Cog
-import datetime
+import mysql.connector
 
 
 class Admin(commands.Cog):
@@ -24,6 +27,35 @@ class Admin(commands.Cog):
             await ctx.send(embed = embed)
         
         return validUser
+
+    @commands.command(name = 'test',
+                      pass_context = True)
+    async def test(self,ctx):
+        print(f"{os.getenv('SQLUser')} -> {os.getenv('SQLPass')} -> {os.getenv('SQLHost')}")
+        mydb = mysql.connector.connect(
+            host = os.getenv('SQLHost'),
+            user = os.getenv('SQLUser'),
+            passwd = os.getenv('SQLPass'),
+            database = 'heroku_2ae506eb86eb6d9'
+        )
+        guild = ctx.guild
+        formattedGuildName = ('_').join([nm.lower() for nm in guild.name.split()])
+
+        myCursor = mydb.cursor()
+        
+        myCursor.execute("SHOW TABLES")
+        tableSet = set()
+        for t in myCursor:
+            tableSet.add(t[0])
+
+        if formattedGuildName not in tableSet:
+            myCursor.execute(f'CREATE TABLE {formattedGuildName} (channelName VARCHAR(50), messageCount INTEGER, date VARCHAR(15))')
+
+        sqlStuff = f"INSERT INTO {formattedGuildName} (channelName, messageCount, date) VALUES (%s, %s, %s)"
+        record = (ctx.channel.name, 15, datetime.date(datetime.now()))
+
+        myCursor.execute(sqlStuff, record)
+        mydb.commit()
 
     @commands.command(name = 'msg_count',
                        pass_context = True,
