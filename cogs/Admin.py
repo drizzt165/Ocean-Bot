17f9bdfdfb8376d778f676418ef1d6c19a836331
Nick Brannies <nick.brannies@gmail.com>
2020-08-14 19:50:04 -0700

    Updated admin cog with cog_check method to check permisions before someone can use these commands.

--- cogs/Admin.py
+++ cogs/Admin.py
@@ -1,14 +1,29 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands import has_permissions
+from discord.ext.commands import has_permissions, Cog
 import datetime
 
-@has_permissions(administrator = True)
+
 class Admin(commands.Cog):
     """Commands only available to admin users."""
+    @has_permissions(administrator = True)
     def __init__(self,client):
         self.client = client
         self.EmbedColour = discord.Colour.red()
+
+    #Check permissions
+    async def cog_check(self, ctx):
+        validUser = ctx.author.guild_permissions.administrator
+        if not validUser:
+            embed = discord.Embed(colour = self.EmbedColour)
+            commandText = str(ctx.message.content)
+            embed.set_author(name = f'You do not have permission to use the command {commandText.split()[0]}')
+            embed.set_footer(text= f"Requested by {ctx.author}",icon_url= ctx.author.avatar_url)
+            
+            await ctx.message.delete()
+            await ctx.send(embed = embed)
+        
+        return validUser
 
     @commands.command(name = 'msg_count',
                        pass_context = True,
@@ -26,7 +41,6 @@ class Admin(commands.Cog):
 
         await ctx.message.delete()
         await ctx.send(embed=embed)
-
 
     @commands.command(name = 'msg_graph',
                       pass_context = True,
@@ -47,7 +61,6 @@ class Admin(commands.Cog):
         
         await ctx.message.delete()
         await ctx.send(embed=embed)
-
 
 def setup(client):
     print("Setting up Admin Cog...")
