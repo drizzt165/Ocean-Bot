c0ed81f2ab46efbed051b23a880221ec5726c4fc
Nick Brannies <nick.brannies@gmail.com>
2020-08-16 19:01:48 -0700

    Removed !msg_count and !msg_graph commands from Admin as they were implemented poorly. ***Soon to be replaced with a Database solution which won't need to iterate over all messages every command call.

--- cogs/Admin.py
+++ cogs/Admin.py
@@ -25,43 +25,6 @@ class Admin(commands.Cog):
         
         return validUser
 
-    @commands.command(name = 'msg_count',
-                       pass_context = True,
-                       description = "Print amount of messages in a channel.")
-    async def msg_count(self,ctx,channel:discord.TextChannel = None):
-        embed = discord.Embed(colour = self.EmbedColour)
-
-        channel = channel or ctx.channel
-        count = 0
-        async for _ in channel.history(limit=None):
-            count += 1
-
-        embed.set_footer(text= f"Requested by {ctx.author}",icon_url= ctx.author.avatar_url)
-        embed.add_field(name = f'Message count:', value = f'{count} messages in {channel.mention}')
-
-        await ctx.message.delete()
-        await ctx.send(embed=embed)
-
-    @commands.command(name = 'msg_graph',
-                      pass_context = True,
-                      description = "Print graph of message history.")
-    async def msg_graph(self,ctx):
-        embed = discord.Embed(colour = self.EmbedColour)
-
-        guild = ctx.guild
-        textChannels = guild.text_channels
-        
-        for c in textChannels:
-            count = 0
-            async for _ in c.history(limit=None):
-                count+=1
-            embed.add_field(name = f'Message count for #{c.name}', value = count, inline = False)
-
-        embed.set_footer(text= f"Requested by {ctx.author}",icon_url= ctx.author.avatar_url)
-        
-        await ctx.message.delete()
-        await ctx.send(embed=embed)
-
 def setup(client):
     print("Setting up Admin Cog...")
     client.add_cog(Admin(client))
